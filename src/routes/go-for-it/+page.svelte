9f8ce13cc1c2ada6bfee55302046ba6ecdbf8bdc
Bryony Dudok de Wit <bryonywatson1@googlemail.com>
2023-03-09 13:57:19 +0000

    copy fixes

--- src/routes/go-for-it/+page.svelte
+++ src/routes/go-for-it/+page.svelte
@@ -2,15 +2,15 @@
 <h2>Fantastic, go for it!</h2>
 <p>Giving positive feedback is very important. To be as helpful as possible, be clear and specific.</p>
 <h3>Positive feedback is important</h3>
-<p>Positive feedback is super important. It helps us know we’re on the right track, and it feels good when our efforts are recognised. Evidence suggests we should be giving around 5 times more positive feedback than negative. </p>
+<p>Positive feedback is super important. It helps us know we're on the right track, and it feels good when our efforts are recognised. Evidence suggests we should be giving around 5 times more positive feedback than negative. </p>
 <h3>Be clear and specific</h3>
-<p>Examples help us understand what we’re doing well, so we can continue. You could describe the:
+<p>Examples help us understand what we're doing well, so we can continue. You could describe the:
 </p>
 <h3>Describe the situation, behaviour and impact</h3>
 <p>To check your intentions and keep feedback specific, try to describe the:
 <GiveFeedback/>
-It could be as simple as ‘Very well structured presentation, with great use of visuals [behaviour] in the weekly wrap just now [situation], really exciting to see what you’re working on and the information was very easy to take in! [impact]’ 
+It could be as simple as 'Very well structured presentation, with great use of visuals, easy to process [behaviour] in the weekly wrap just now [situation], really exciting to see how your work contributes to our goals! [impact]' 
 Be as specific as you can with the [behaviour] to help them really understand what they can do more of.
 This is called the SBI model. An alternative is COIN.  </p>
 <h3>Make it timely</h3>
-<p>It helps if the feedback is timely – soon after the event, so it’s fresh in the person’s mind.</p>
+<p>It helps if the feedback is timely – soon after the event, so it's fresh in the person's mind.</p>
