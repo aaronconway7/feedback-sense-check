08a038f5900c101d6dc3a1e201445968f05c4a26
Alex Callard <alex.callard@thriva.co>
2023-03-09 13:05:42 +0000

    Start question asking logic

--- src/lib/components/Question.svelte
+++ src/lib/components/Question.svelte
@@ -2,10 +2,17 @@
   import Button from "./Button.svelte";
   export let question="Question?"
   export let options=[]
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher()
+
+  function clickOption(option) {
+    dispatch('clickOption', option);
+  }
 </script>
 
 <h2>{question}</h2>
 {#each options as option}
-  <Button label="{option.text}" variant="inactive" />
+  <Button label="{option.text}" variant="inactive" on:click={() => clickOption(option)} />
 {/each}
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,37 @@
 <script>
   import Button from "../lib/components/Button.svelte";
 	import Question from "../lib/components/Question.svelte";
-  import questions from "../lib/questions.json";
+  import sourceQuestions from "../lib/questions.json";
+
+  let questions = [];
+
+  function findQuestion(byId) {
+    return sourceQuestions.find(q => q.id === byId);
+  }
+
+  function handleAnswer(e) {
+    console.log(e.detail);
+    const answer = e.detail;
+    appendQuestion(answer.goTo);
+  }
+
+  function appendQuestion(questionId) {
+    let nextQuestion = findQuestion(questionId);
+    questions = [...questions, nextQuestion];
+  }
+
+  function start() {
+    questions = [];
+    questions = [...questions, sourceQuestions[0]];
+  }
 </script>
 <h1 class="text-7xl font-sans">Feedback <span class="font-serif">Sense Check</span></h1>
 <div class="grid py-3 mb-4">
 <p>Feedback is a gift, but it can trigger emotions which cause us to make irrational choices. </p>
 <p>Before giving or responding to feedback at work, use Feedback Sense Check to help you manage your emotions.</p>
 </div>
-<Button variant="active" label="Let's Go" />
+<Button variant="active" label="Let's Go" on:click={start}/>
 <hr class="my-8 mt-8" />
-<Question question="{questions[0].question}" options="{questions[0].options}" />
+{#each questions as question}
+<Question question="{question.question}" options="{question.options}" on:clickOption={handleAnswer} />
+{/each}
