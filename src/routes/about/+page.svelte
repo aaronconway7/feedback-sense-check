1ef6494ddf9f7185fcf04c702c03a832e65135c2
tomhiskey <43188299+tomhiskey@users.noreply.github.com>
2023-03-09 14:47:36 +0000

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,43 @@
 <h1>About</h1>
+
+<p>“Withholding feedback is choosing comfort over growth. 
+  Staying silent deprives people of the opportunity to learn. 
+  In healthy relationships, honesty is an expression of care.”</p>
+<p>– Adam Grant, Psychologist</p>
+  
+ <h3>Helping develop the people we care about</h3>
+
+<p>A healthy feedback loop builds awareness and helps people improve. We have a responsibility to develop the people we care about.
+<p>But when we receive feedback, a flood of neurochemicals activated in the amygdala (where emotions are predominantly processed) </p>
+  limits our abilities to activate other parts of the brain typically used for higher/executive function – reducing our ability to process / think logically. 
+<p>As a result, we can make irrational choices, give feedback poorly, and break the feedback loop.</p>
+  Feedback Sense Check is a questionnaire-style web flow to help you manage your emotions before giving – or after receiving – feedback. </p>
+<p>It’s for anyone working in an organisation to build empathy, maintain a healthy feedback loop and help people develop.</p>
+
+<h3>Biases and perspectives in feedback</h3>
+
+<p>When we perceive, we take in sensory information and mesh it with what we know about the world, 
+  which can sometimes be based on limited or exceptional information; hence biases. 
+  This is why perspectives often differ - not only do we collect different sensory data from literally different perspectives 
+  but we also add our own unique perspective to this process to create ‘our truth’ on what happened. </p>
+  <p>This is what feedback aims to reveal. How our perspectives differ. </p>
+  <p>And there is no ‘right’ way to look at something but there can be learning for both of you.</p>
+
+<h3>Why I created Feedback Sense Check</h3>
+<p>“I’ve given feedback badly, too many times. Once I gave feedback at 4pm on a Friday while I was feeling grumpy – it landed badly and caused someone I care about to feel bad over the weekend.
+I came up with the idea for Feedback Sense Check in 2022 to avoid that kind of situation. It’s a logical friend to help manage emotions and give feedback in a better way.
+  It started as a side project, before collaborating with Simon and others at Thriva.”</p>
+<p>- Tom Hiskey, design manager</p>
+
+<h3>Built during Thriva Maker Day</h3>
+v1 of Feedback Sense Check was built in one day, 9 March 2023, by some of the amazing team at Thriva.
+
+<h3>The team</h3>
+<p>Aaron Conway – engineering</p>
+<p>Alex Callard – engineering</p>
+<p>Bryony Watson – engineering</p>
+<p>Ela Tuns – engineering and content</p>
+<p>Emma Buckee – illustrations</p>
+<p>Simon Meekings – content</p>
+<p>Tom Hiskey – content and design</p>
+
