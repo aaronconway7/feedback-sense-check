8cf6e1585121a2fcbf9794a196e4c13d1627dd12
Alex Callard <alex.callard@thriva.co>
2023-03-09 12:32:04 +0000

    Starts question component

---
+++ src/lib/components/Question.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Button from "./Button.svelte";
+  export let question="Question?"
+  export let options=[]
+</script>
+
+<h2>{question}</h2>
+{#each options as option}
+  <Button label="{option.label}" variant="inactive" />
+{/each}
+
