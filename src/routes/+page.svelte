58de74b329bb6fcb9aa84fb91973851d5b6d1357
Aaron Conway <aaronconway796@gmail.com>
2023-03-09 14:05:17 +0000

    with illustration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import Button from "../lib/components/Button.svelte";
 	import Question from "../lib/components/Question.svelte";
   import sourceQuestions from "../lib/questions.json";
+  import Illustration from '$lib/assets/img/illustration.svg';
 
   let questions = [];
 
@@ -25,10 +26,15 @@
     questions = [...questions, sourceQuestions[0]];
   }
 </script>
-<h1 class="text-7xl font-sans">Feedback <span class="font-serif">Sense Check</span></h1>
-<div class="grid py-3 mb-4">
-<p>Feedback is a gift, but it can trigger emotions which cause us to make irrational choices. </p>
-<p>Before giving or responding to feedback at work, use Feedback Sense Check to help you manage your emotions.</p>
+
+<img class="h-40 mb-7" src={Illustration} alt="Feedback Sense Check Illustration" />
+<h1 class="text-7xl font-sans mb-7">Feedback <span class="font-serif">Sense Check</span></h1>
+<div class="grid gap-4 mb-7">
+	<p>Feedback is a gift, but it can trigger emotions which cause us to make irrational choices.</p>
+	<p>
+		Before giving or responding to feedback at work, use Feedback Sense Check to help you manage
+		your emotions.
+	</p>
 </div>
 <Button variant="active" label="Let's Go" on:click={start}/>
 <hr class="my-8 mt-8" />
