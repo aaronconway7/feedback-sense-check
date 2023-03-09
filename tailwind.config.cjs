12aca9dfa39c52fe2b84f9253dff4254ec9b1ff4
Aaron Conway <aaronconway796@gmail.com>
2023-03-09 12:23:28 +0000

    fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -7,6 +9,10 @@ module.exports = {
 			navy: { DEFAULT: '#202634', 90: '#353A47' },
 			orange: '#D59640',
 			grey: '#F0F0F0'
+		},
+		fontFamily: {
+			sans: ['Satoshi', ...defaultTheme.fontFamily.sans],
+			serif: ['Meriweather', ...defaultTheme.fontFamily.serif]
 		}
 	},
 	plugins: []
