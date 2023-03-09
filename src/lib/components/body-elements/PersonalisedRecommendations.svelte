0754f83d0a2259b87b4bcf5dd8fc451812cf48f8
Aaron Conway <aaronconway796@gmail.com>
2023-03-09 16:27:48 +0000

    personalised recs

---
+++ src/lib/components/body-elements/PersonalisedRecommendations.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { isFriday, isWeekend, getHours } from 'date-fns';
+	import { answers } from '../../../stores';
+	import PauseBefore from '../body-elements/personalised-recommendations/pause-before.svelte';
+	import GoodPlace from '../body-elements/personalised-recommendations/good-place.svelte';
+	import TimingRight from '../body-elements/personalised-recommendations/timing-right.svelte';
+	import TimingWrong from '../body-elements/personalised-recommendations/timing-wrong.svelte';
+	import MoreSenior from '../body-elements/personalised-recommendations/more-senior.svelte';
+	import UpwardFeedback from '../body-elements/personalised-recommendations/upward-feedback.svelte';
+	import PeerFeedback from '../body-elements/personalised-recommendations/peer-feedback.svelte';
+	import GoodRelationship from './personalised-recommendations/good-relationship.svelte';
+	import MaximiseCuriosity from './personalised-recommendations/maximise-curiosity.svelte';
+
+	$: isEarly = getHours(new Date()) >= 2 && getHours(new Date()) < 18;
+	$: isEndOfWeek = isFriday(new Date()) || isWeekend(new Date());
+</script>
+
+{#if $answers.find((a) => a.questionId === '7' && ['b', 'c'].includes(a.optionId))}
+	<PauseBefore />
+{/if}
+{#if $answers.find((a) => a.questionId === '7' && ['a'].includes(a.optionId))}
+	<GoodPlace />
+{/if}
+
+{#if isEarly && !isEndOfWeek}
+	<TimingRight />
+{/if}
+{#if !isEarly || isEndOfWeek}
+	<TimingWrong />
+{/if}
+
+{#if $answers.find((a) => a.questionId === '5' && ['a'].includes(a.optionId))}
+	<MoreSenior />
+{/if}
+{#if $answers.find((a) => a.questionId === '5' && ['b'].includes(a.optionId))}
+	<UpwardFeedback />
+{/if}
+{#if $answers.find((a) => a.questionId === '5' && ['c'].includes(a.optionId))}
+	<PeerFeedback />
+{/if}
+
+{#if $answers.find((a) => a.questionId === '6' && ['a'].includes(a.optionId))}
+	<GoodRelationship />
+{/if}
+{#if $answers.find((a) => a.questionId === '6' && ['b', 'c'].includes(a.optionId))}
+	<MaximiseCuriosity />
+{/if}
--- src/lib/components/body-elements/personalised-recommendations/timing-right.svelte
+++ src/lib/components/body-elements/personalised-recommendations/timing-right.svelte
@@ -1,2 +1,13 @@
+<script>
+	import { format } from 'date-fns';
+	let day = format(new Date(), 'EEEE');
+	let time = format(new Date(), 'HH:mm');
+</script>
+
 <h3 class="text-3xl mb-8">The timing seems right</h3>
-<p class="text-lg mb-8">Right now it's {time} on a {day}. Assuming your colleague works standard weekday hours, now could be a good time to give feedback. If they work different hours, consider – is this a good time to feed back, or is it late in their working day / a non-working day? If so, they might not be in a great headspace to receive feedback.</p>
+<p class="text-lg mb-8">
+	Right now it's {time} on a {day}. Assuming your colleague works standard weekday hours, now could
+	be a good time to give feedback. If they work different hours, consider – is this a good time to
+	feed back, or is it late in their working day / a non-working day? If so, they might not be in a
+	great headspace to receive feedback.
+</p>
