ef0df426a01f00c1783f5364db671cf72a96c95e
Alex Callard <alex.callard@thriva.co>
2023-03-09 12:31:52 +0000

    Adds content styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,11 @@
 <script>
   import Button from "../lib/components/Button.svelte";
-  function clickMe() {
-    alert('You clicked me');
-  }
+	import Question from "../lib/components/Question.svelte";
 </script>
-<h1 class="text-7xl">Feedback <span>Sense Check</span></h1>
+<h1 class="text-7xl font-sans">Feedback <span class="font-serif">Sense Check</span></h1>
 <div class="grid py-3 mb-4">
 <p>Feedback is a gift, but it can trigger emotions which cause us to make irrational choices. </p>
 <p>Before giving or responding to feedback at work, use Feedback Sense Check to help you manage your emotions.</p>
 </div>
-<Button variant="active" label="Let's Go" on:click={clickMe} />
+<Button variant="active" label="Let's Go" />
 <hr class="my-8 mt-8" />
