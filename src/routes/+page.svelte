512d132486aae2f7b72ed3d93a70c6afb4a34021
Alex Callard <alex.callard@thriva.co>
2023-03-09 12:22:23 +0000

    Content main page, logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,10 @@
     alert('You clicked me');
   }
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-7xl">Feedback <span>Sense Check</span></h1>
+<div class="grid py-3 mb-4">
+<p>Feedback is a gift, but it can trigger emotions which cause us to make irrational choices. </p>
+<p>Before giving or responding to feedback at work, use Feedback Sense Check to help you manage your emotions.</p>
+</div>
 <Button variant="active" label="Let's Go" on:click={clickMe} />
-<Button variant="outlined" size="small" label="I've received feedback" on:click={clickMe} />
+<hr class="my-8 mt-8" />
