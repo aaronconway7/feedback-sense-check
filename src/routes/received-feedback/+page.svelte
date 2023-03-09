2f1df0facb988e6ccaba6a0fb122fb9ddd689c22
Bryony Dudok de Wit <bryonywatson1@googlemail.com>
2023-03-09 15:58:22 +0000

    add illustration

--- src/routes/received-feedback/+page.svelte
+++ src/routes/received-feedback/+page.svelte
@@ -1,2 +1,6 @@
+<script>
+    import Illustration from '$lib/assets/img/illustration.svg';
+</script>
+<img class="h-40 mb-7" src={Illustration} alt="Feedback Sense Check Illustration" />
 <h2 class="text-6xl mb-8">You've received feedback!</h2>
 <p class="text-2xl mb-8">This page is a work in progress...</p>
